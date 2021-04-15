d10639df2d3b9a94a88579e6c0363adf870fda34
skalatski <searhei.kalatski@idt.net>
2021-04-15 20:40:13 +0300

    Small changes

--- src/test/java/PetTest.java
+++ src/test/java/PetTest.java
@@ -4,7 +4,6 @@ import models.*;
 import lombok.SneakyThrows;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
